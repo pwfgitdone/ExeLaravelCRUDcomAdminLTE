3faf78c0d5fc8a95c87276bcd6e112aee87dfbde
pwfgitdone <ddrdan7@hotmail.com>
2022-07-21 14:53:53 -0300

    Cliente acessível somente com login

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -8,6 +8,10 @@ use GuzzleHttp\Client;
 
 class ClienteController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
